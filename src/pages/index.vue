f6af050e31f7cccb3ce3660d2fbbf9cd6a9f26e2
PsyFalos <sifamail@foxmail.com>
2025-03-30 19:30:22 +0800

    jump

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,4 +1,11 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+const router = useRouter()
+router.replace('/recent-formulas') // 自动跳转到目标路由
+</script>
+
 <template>
   <Logos my="4" />
   <HelloWorld msg="Hello Vue 3 + Element Plus + Vite" />
+  <div class="p-4 text-gray-400">正在跳转...</div>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  
   resolve: {
     alias: {
       '~/': `${path.resolve(__dirname, 'src')}/`,
